e3f877a106f3647dcd69b66b91f8298709a846ee
Atanas Pamukchiev <atanaspam@users.noreply.github.com>
2023-02-12 21:53:34 +0100

    fix: Fix missing examples documentation

--- examples/ip_whitelist/delete_ip_whitelist_entries.go
+++ examples/ip_whitelist/delete_ip_whitelist_entries.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	var DeploymentNameFlag = flag.String("deployment_name", "", "The Splunk deployment name")
-	var TargetfeatureFlag = flag.String("target_feature", "", "The feature which will be targeted")
+	var TargetfeatureFlag = flag.String("target_feature", "", "The feature you want to request IP Whitelists for. See https://docs.splunk.com/Documentation/SplunkCloud/9.0.2208/Config/ConfigureIPAllowList#Determine_IP_allow_list_use_case")
 	var SubnetToDeleteFlag = flag.String("subnet", "", "The subnet to be added (in CIDR notation). Ex: 123.123.123.123/32")
 	flag.Parse()
 	acsClient, err := splunkacs.NewClient(*DeploymentNameFlag, os.Getenv("SPLUNK_AUTH_TOKEN"))
